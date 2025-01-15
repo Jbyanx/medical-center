fb8d17b5c0a5705cea937eabcae3ce92088d62be
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-01-15 03:53:27 -0500

    Controlador con el Crud de medicos

--- src/main/java/med/vol/api/controller/MedicoController.java
+++ src/main/java/med/vol/api/controller/MedicoController.java
@@ -1,15 +1,23 @@
 package med.vol.api.controller;
 
-import med.vol.api.dto.DatosMedico;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.vol.api.dto.DatosActualizacionMedico;
+import med.vol.api.dto.DatosListadoMedico;
+import med.vol.api.dto.DatosRegistroMedico;
 import med.vol.api.entity.Medico;
 import med.vol.api.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/medicos")
@@ -18,14 +26,83 @@ public class MedicoController {
     private MedicoRepository medicoRepository;
 
     @GetMapping
-    public ResponseEntity<List<Medico>> getAllMedicos() {
-        return ResponseEntity.ok(medicoRepository.findAll());
+    public ResponseEntity<Page<DatosListadoMedico>> getAllMedicos(
+        @PageableDefault(size = 2)
+            Pageable pageable
+    ) {
+        //Page<DatosListadoMedico> datosListadoMedicos = medicoRepository.findAll(pageable).map(DatosListadoMedico::new);
+        Page<DatosListadoMedico> datosListadoMedicos = medicoRepository.findByIsActivoTrue(pageable)
+                .map(DatosListadoMedico::new);
+        return ResponseEntity.ok(datosListadoMedicos);
+    }
+
+    @GetMapping("/{idMedico}")
+    public ResponseEntity<DatosListadoMedico> getMedicoById(@PathVariable Long idMedico) {
+        Optional<Medico> medico = medicoRepository.findById(idMedico);
+
+        if (medico.isPresent()) {
+            DatosListadoMedico datosMedico = new DatosListadoMedico(medico.get());
+
+            return ResponseEntity.ok(datosMedico);
+        } else{
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping
-    public ResponseEntity<Medico> createMedico(@RequestBody DatosMedico datosMedico) {
-        Medico medicoSaved = medicoRepository.save(new Medico(datosMedico));
+    public ResponseEntity<DatosListadoMedico> createMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico) {
+        Medico medicoSaved = medicoRepository.save(new Medico(datosRegistroMedico));
 
-        return ResponseEntity.ok(medicoSaved);
+        DatosListadoMedico datosListadoMedico = new DatosListadoMedico(medicoSaved);
+
+        URI newMedico = ServletUriComponentsBuilder.fromCurrentRequest().path("/{medicoId}")
+                .buildAndExpand(medicoSaved.getId()).toUri();
+
+        return ResponseEntity.created(newMedico).body(datosListadoMedico);
+    }
+
+    @PutMapping("/{idMedico}")
+    @Transactional
+    public ResponseEntity<DatosListadoMedico> updateMedico(@RequestBody @Valid DatosActualizacionMedico datosActualizacionMedico, @PathVariable Long idMedico) {
+
+        return medicoRepository.findById(idMedico)
+                .map(m -> {
+
+                    m.actualizarDatos(datosActualizacionMedico);
+                    DatosListadoMedico datosListadoMedico = new DatosListadoMedico(m);
+
+                    return ResponseEntity.ok(datosListadoMedico);
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    //ELIMINADO FISICO
+//    @DeleteMapping("/{idMedico}")
+//    @Transactional
+//    public ResponseEntity<?> deleteMedico(@PathVariable Long idMedico) {
+//        Optional<Medico> medicoToDelete = medicoRepository.findById(idMedico);
+//
+//        if (medicoToDelete.isPresent()) {
+//            medicoRepository.deleteById(idMedico);
+//            return ResponseEntity.ok().build();
+//        }else {
+//            return ResponseEntity.notFound().build();
+//        }
+//    }
+
+    //eliminado logico
+    @DeleteMapping("/{idMedico}")
+    @Transactional
+    public ResponseEntity<?> desactivateMedico(@PathVariable Long idMedico) {
+        Optional<Medico> medicoToDesactivate = medicoRepository.findById(idMedico);
+
+        if (medicoToDesactivate.isPresent()) {
+            Medico medico = medicoToDesactivate.get();
+
+            medico.desactivar();
+            return ResponseEntity.ok().build();
+        }else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
