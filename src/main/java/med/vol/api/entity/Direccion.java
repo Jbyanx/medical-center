000eecf89cb26eb815043a12a7366aabb390e2c4
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-01-15 03:53:06 -0500

    Entidades

--- src/main/java/med/vol/api/entity/Direccion.java
+++ src/main/java/med/vol/api/entity/Direccion.java
@@ -1,12 +1,9 @@
 package med.vol.api.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import jakarta.validation.constraints.NotNull;
 import med.vol.api.dto.DatosDireccion;
 
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
 @Embeddable
 public class Direccion {
     private String calle;
@@ -14,10 +11,41 @@ public class Direccion {
     private String ciudad;
     private String complemento;
 
+    public Direccion() {
+    }
+
     public Direccion(DatosDireccion direccion) {
         this.calle = direccion.calle();
         this.distrito = direccion.distrito();
         this.ciudad = direccion.ciudad();
         this.complemento = direccion.complemento();
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public String getDistrito() {
+        return distrito;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    @Override
+    public String toString() {
+        return "calle='" + calle + '\'' +
+                ", distrito='" + distrito + '\'' +
+                ", ciudad='" + ciudad + '\'' +
+                ", complemento='" + complemento;
+    }
+
+    public Direccion actualizarDatos(@NotNull DatosDireccion direccion) {
+        return new Direccion(direccion);
     }
 }
--- src/main/java/med/vol/api/entity/Medico.java
+++ src/main/java/med/vol/api/entity/Medico.java
@@ -1,32 +1,109 @@
 package med.vol.api.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
-import med.vol.api.dto.DatosMedico;
+import jakarta.validation.Valid;
+import med.vol.api.dto.DatosActualizacionMedico;
+import med.vol.api.dto.DatosRegistroMedico;
+import org.springframework.util.StringUtils;
+
+import java.util.Objects;
 
 @Entity
 @Table(name = "medicos")
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode(of = "id")
 public class Medico {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
     private String email;
+    private String telefono;
     private String documento;
-    @Enumerated(EnumType.STRING)
     private Especialidad  especialidad;
     @Embedded
     private Direccion direccion;
+    @Column(name = "is_activo")
+    private Boolean isActivo;
 
-    public Medico(DatosMedico datosMedico) {
-        this.nombre = datosMedico.nombre();
-        this.email = datosMedico.email();
-        this.documento = datosMedico.documento();
-        this.especialidad = datosMedico.especialidad();
-        this.direccion = new Direccion(datosMedico.direccion());
+    public Medico(DatosRegistroMedico datosRegistroMedico) {
+        this.nombre = datosRegistroMedico.nombre();
+        this.email = datosRegistroMedico.email();
+        this.documento = datosRegistroMedico.documento();
+        this.telefono = datosRegistroMedico.telefono();
+        this.especialidad = datosRegistroMedico.especialidad();
+        this.direccion = new Direccion(datosRegistroMedico.direccion());
+        this.isActivo = true;
+    }
+
+    public Medico() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+    public Boolean getisActivo() {return isActivo;}
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public Especialidad getEspecialidad() {
+        return especialidad;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Medico medico)) return false;
+        return Objects.equals(id, medico.id) && Objects.equals(nombre, medico.nombre) && Objects.equals(email, medico.email) && Objects.equals(telefono, medico.telefono) && Objects.equals(documento, medico.documento) && especialidad == medico.especialidad && Objects.equals(direccion, medico.direccion);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return  "  nombre='" + nombre + '\'' +
+                ", email='" + email + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", documento='" + documento + '\'' +
+                ", especialidad=" + especialidad +
+                ", direccion=" + direccion;
+    }
+
+    public void actualizarDatos(@Valid DatosActualizacionMedico datosActualizacionMedico) {
+        if(StringUtils.hasText(datosActualizacionMedico.nombre())){
+            this.nombre = datosActualizacionMedico.nombre();
+        }
+        if(StringUtils.hasText(datosActualizacionMedico.telefono())){
+            this.telefono = datosActualizacionMedico.telefono();
+        }
+        if(StringUtils.hasText(datosActualizacionMedico.documento())) {
+            this.documento = datosActualizacionMedico.documento();
+        }
+        if (datosActualizacionMedico.direccion() != null) {
+            this.direccion = this.direccion.actualizarDatos(datosActualizacionMedico.direccion());
+        }
+    }
+
+    public void desactivar() {
+        this.isActivo = false;
     }
 }
