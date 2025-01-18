86e9975d51f08e67ebb7c7c035556fae96db7208
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-01-18 13:27:05 -0500

    repositorio del usuario con el query method findbyUsername, en nuestro caso login

---
+++ src/main/java/med/vol/api/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package med.vol.api.repository;
+
+import med.vol.api.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String username);
+}
